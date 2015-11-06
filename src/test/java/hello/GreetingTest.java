cf149faf2d6697b85b57d6d86e4c7a2a15776333
Seshagiri Nagineni <sesh_nagineni@yahoo.com>
2015-11-06 16:28:55 -0500

    test10

--- src/test/java/hello/GreetingTest.java
+++ src/test/java/hello/GreetingTest.java
@@ -11,16 +11,18 @@ public class GreetingTest {
 	private static Greeting greeting;
 
 	@BeforeClass
-	public static void initCalculator() {
-		greeting = new Greeting(7, "abc");
+	public static void initGreeting() {
+		
 	}
 
 	@Test
 	public void test() {
 		System.out.println("Default Test ");
-		
+		greeting = new Greeting(7, "abc");
 		
 		assertEquals(greeting.getId(), 7);
+		
+		assertEquals(greeting.getContent(), "abc");
 	}
 
 }
